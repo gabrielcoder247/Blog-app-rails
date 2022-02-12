e472b8ac9066bd3ef9898b4742c8622775d9f7ff
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-02-12 07:30:49 +0100

    Add models

---
+++ test/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def admin?
+    role == 'admin'
+  end
+
+  def recently_posted
+    posts.order(created_at: :desc).limit(3)
+  end
+end
