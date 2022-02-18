9228cb0015c871969ccee3f743e0ed2f4febd93e
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-02-18 16:41:40 +0100

    Edit migrations Add type

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,6 @@
 ActiveRecord::Schema[7.0].define(version: 2022_02_17_092351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  # create_table "users", force: :cascade do |t|
-  #   t.string "name"
-  #   t.string "photo"
-  #   t.text "bio"
-  #   t.integer "PostsCounter"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
   create_table "comments", force: :cascade do |t|
     t.text "text"
     t.datetime "created_at", null: false
@@ -44,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_17_092351) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "text"
-    t.integer "CommentsCounter"
+    t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
