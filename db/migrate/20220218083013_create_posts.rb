727a065ee3ef1e2e57f84ec4f31bda450444706c
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-02-18 09:34:23 +0100

    update schema

--- db/migrate/20220218083013_create_posts.rb
+++ db/migrate/20220218083013_create_posts.rb
@@ -2,8 +2,9 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :text
-      t.integer :CommentsCounter
+      t.text :text
+      t.string :comments_counter
+      t.integer :likes_counter
 
       t.timestamps
     end
