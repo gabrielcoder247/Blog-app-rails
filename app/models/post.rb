55d7d0d8f9a26d4559d856c2fe7814ae387fc7bd
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-02-23 12:10:23 +0100

    Add Views and  controller to index and show

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   after_save :post_counter
 
   def five_recent_comments
-    comment.limit(5).order(created_at: :desc)
+    comments.limit(5).order(created_at: :desc)
   end
 
   private
