65e6303d1c266108c11215cba2e6eaa5fe356d14
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-02-23 11:40:24 +0100

    Add view templates for index and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,22 @@
+
 class PostsController < ApplicationController
   def index
-    index
+    @params = params
+    @user = User.find_by(id: params[:user_id])
+    @posts = Post.all.where(author_id: params[:user_id])
+    @comments = Comment.all
+    @likes = Like.all
+    Post.all.each do |post|
+      post.update(CommentsCounter: Comment.where(post_id: post.id).count)
+      post.update(likes_counter: Like.where(posts_id: post.id).count)
+    end
   end
 
   def show
-    show
+    @comments = Comment.all.order(created_at: :desc)
+    @post = Post.find_by(author_id: params[:user_id], id: params[:id])
+    @user = User.find_by(id: params[:user_id])
+    @users = User.all
+    @likes = Like.all
   end
 end
