a1d10c1c958e1e9f83d6ace56d7cf9e83ba99134
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-02-25 08:26:38 +0100

    Add methods to create and update forms in the controllers, post and comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,28 @@ class PostsController < ApplicationController
     @users = User.all
     @likes = Like.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(author_id: params[:user_id], title: params[:title], text: params[:text])
+    redirect_back(fallback_location: root_path)
+    flash.alert = if @post.save
+                    'Comment posted...'
+                  else
+                    'Comment failed...'
+                  end
+  end
+
+  def edit; end
+
+  def update; end
+
+  private
+
+  def user_params
+    params.require(:post).permit(:title, :text)
+  end
 end
