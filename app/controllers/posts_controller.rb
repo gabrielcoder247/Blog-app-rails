c8a873086619b8fdc04547cca2b8e1aa48c87723
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-03-01 15:06:04 +0100

    Fix conflict-2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 
 class PostsController < ApplicationController
   def index
+  # index function
     @user = User.find_by(id: params[:user_id])
     @posts = Post.all.where(author_id: params[:user_id])
     @comments = Comment.all.includes(:post_id)
