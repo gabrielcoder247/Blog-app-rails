3d6acd78319364dccda0cc108a5a243c5dc10851
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-03-01 10:06:52 +0100

    generate devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,18 @@
+
 class PostsController < ApplicationController
   def index
-    @params = params
     @user = User.find_by(id: params[:user_id])
     @posts = Post.all.where(author_id: params[:user_id])
-    @comments = Comment.all
-    @likes = Like.all
-    Post.all.each do |post|
-      post.update(CommentsCounter: Comment.where(post_id: post.id).count)
-      post.update(likes_counter: Like.where(posts_id: post.id).count)
-    end
+    @comments = Comment.all.includes(:post_id)
+    @likes = Like.all.includes(:post_id)
   end
 
   def show
     @comments = Comment.all.order(created_at: :desc)
     @post = Post.find_by(author_id: params[:user_id], id: params[:id])
     @user = User.find_by(id: params[:user_id])
-    @users = User.all
-    @likes = Like.all
+    @users = User.all.includes(:name, :id)
+    @likes = Like.all.includes(:post_id)
   end
 
   def new
@@ -27,9 +23,9 @@ class PostsController < ApplicationController
     @post = Post.new(author_id: params[:user_id], title: params[:title], text: params[:text])
     redirect_back(fallback_location: root_path)
     flash.alert = if @post.save
-                    'Comment posted...'
+                    'Post created...'
                   else
-                    'Comment failed...'
+                    "Post failed, #{@post.errors.full_messages}"
                   end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,36 @@
+# class UsersController < ApplicationController
+#   def index
+#     @user = User.all
+#     @posts = Post.all
+#   end
+
+#   def show
+#     Post.all.each do |post|
+#       post.update(CommentsCounter: Comment.where(post_id: post.id).count)
+#       post.update(likes_counter: Like.where(posts_id: post.id).count)
+#     end
+#     @total_posts = Post.all.where(author_id: params[:id]).count
+#     @likes = Like.all
+#     @comments = Comment.all.order(created_at: :desc)
+#     @user = User.find_by(id: params[:id])
+#   end
+# end
+
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    user_signed_in?
+    current_user
+    user_session
     @user = User.all
     @posts = Post.all
   end
 
   def show
-    Post.all.each do |post|
-      post.update(CommentsCounter: Comment.where(post_id: post.id).count)
-      post.update(likes_counter: Like.where(posts_id: post.id).count)
-    end
-    @total_posts = Post.all.where(author_id: params[:id]).count
-    @likes = Like.all
-    @comments = Comment.all.order(created_at: :desc)
+    @likes = Like.all.includes(:post_id)
+    @comments = Comment.all.includes(:post_id).order(created_at: :desc)
     @user = User.find_by(id: params[:id])
   end
 end
+
